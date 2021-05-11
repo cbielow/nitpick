6f72346f8a17c53aeb4109a9064748e191a17cb5
bielow <chris.bielow@fu-berlin.de>
2021-05-11 11:00:56 +0200

    fixup amsmercury onload (.First.lib is deprecated since R 2.14....)

--- amsmercury/R/zzz.R
+++ amsmercury/R/zzz.R
@@ -1,10 +1,10 @@
 .packageName <- "amsmercury";
 
-.First.lib <- function(libname, pkgname) {
-	#cat("ams.mercury - Copyright (c) 2006 Marc Kirchner\nBased on 'emass' (Haimi P and Rockwood AL, 2006)\n");
+.onLoad <- function(libname, pkgname) {
+	cat("ams.mercury - Copyright (c) 2006 Marc Kirchner\nBased on 'emass' (Haimi P and Rockwood AL, 2006)\n");
 	library.dynam("amsmercury", pkgname, libname);
 }
 
-.Last.lib <- function(libpath) {
+.onUnload <- function(libpath) {
 	library.dynam.unload("amsmercury");
 }
